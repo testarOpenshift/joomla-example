848dd2256e967ee479913a36ece4dd32db7d274c
unknown <fredrik.lehtonen@intraservice.goteborg.se>
2014-08-21 09:16:44 +0200

    ordnat lite

--- .openshift/action_hooks/joomla.sql
+++ .openshift/action_hooks/joomla.sql
@@ -1,11 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `joomlatest` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `joomlatest`;
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
---
--- Host: 127.0.0.1    Database: joomlatest
--- ------------------------------------------------------
--- Server version	5.5.37
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
